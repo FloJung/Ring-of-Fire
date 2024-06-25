8e5a9af97181846361c181c9537d804dd187eddd
FloJung <flo-fj@gmx.at>
2024-06-25 17:13:45 +0200

    Completed project with firebase problems

--- src/app/edit-player/edit-player.component.ts
+++ src/app/edit-player/edit-player.component.ts
@@ -2,7 +2,11 @@ import { NgFor, NgIf, NgStyle } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { MatDialogClose, MatDialogModule, MatDialogRef} from '@angular/material/dialog';
+import {
+  MatDialogClose,
+  MatDialogModule,
+  MatDialogRef,
+} from '@angular/material/dialog';
 import { GameComponent } from '../game/game.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -10,26 +14,37 @@ import { MatInputModule } from '@angular/material/input';
 @Component({
   selector: 'app-edit-player',
   standalone: true,
-  imports: [NgFor,MatDialogClose, MatButtonModule,FormsModule,
+  imports: [
+    NgFor,
+    MatDialogClose,
+    MatButtonModule,
+    FormsModule,
     GameComponent,
     MatDialogModule,
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
-    MatDialogClose],
+    MatDialogClose,
+  ],
   templateUrl: './edit-player.component.html',
-  styleUrl: './edit-player.component.scss'
+  styleUrl: './edit-player.component.scss',
 })
 export class EditPlayerComponent {
   readonly dialogRef = inject(MatDialogRef<EditPlayerComponent>);
-  allProfilePicture = ['profile_2.png', 'profile_1.png'];
+  allProfilePicture = [
+    'profile_2.png', 
+    'profile_1.png',
+    'profile_3.png',
+    'profile_4.png',
+    'profile_5.png',
+    'profile_6.png',
+    'profile_7.jpg',
+    'profile_8.png',
+  ];
 
-  selectPicture() {
-
-  }
+  selectPicture() {}
 
   onNoClick(): void {
     this.dialogRef.close();
-    }
-
+  }
 }
